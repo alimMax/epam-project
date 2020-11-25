c4d3f2be4e1ff2849751ef43bc53e88ae5e37ed0
Alimkhan Maksotov <alimkhan.maksotov@btsdigital.kz>
2020-11-25 22:58:28 +0600

    [ADDED] cross-browser testing

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -1,36 +1,17 @@
 package utils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class DriverManager {
     protected  WebDriver driver;
 
-    @SneakyThrows
     @BeforeEach
     public void setupDriver() {
-        /*WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();*/
-
-        String selenoidUrl = "http://localhost:4444/wd/hub";
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName(System.getProperty("browser"));
-        capabilities.setVersion(System.getProperty("version"));
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("screenResolution", "1280x1024");
-        capabilities.setCapability("enableVideo", false);
-        capabilities.setCapability("enableLog", true);
-
-        driver = new RemoteWebDriver(new URL(selenoidUrl), capabilities);
+        driver = DriverFactory.createDriver(System.getProperty("platform"));
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
